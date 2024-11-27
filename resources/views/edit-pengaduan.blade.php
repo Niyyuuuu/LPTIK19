3312a4ec3cccabe136ad2795fb6f83839e877210
Niyyuuu <nioghoniyyu@gmail.com>
2024-11-27 18:29:49 +0700

    commit wysiwyg

--- resources/views/edit-pengaduan.blade.php
+++ resources/views/edit-pengaduan.blade.php
@@ -7,6 +7,7 @@
     <title>Edit Pengaduan</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="icon" href="{{ asset('img/logo-kemhan.png') }}" type="image/png">
+    <script src="https://cdn.ckeditor.com/4.21.0/standard/ckeditor.js"></script>
     <style>
         @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap');
         body {
@@ -172,6 +173,34 @@
 </form>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js"></script>
+    <script>
+        document.getElementById('lampiran').addEventListener('change', function() {
+        const fileInput = document.getElementById('lampiran');
+        const errorMsg = document.getElementById('file-error');
+        const maxSize = 5 * 1024 * 1024; // Maksimal ukuran file 5MB
+
+        // Ambil file yang diunggah
+        const file = fileInput.files[0];
+
+        if (file && file.size > maxSize) {
+            // Tampilkan pesan error jika ukuran file melebihi 5MB
+            errorMsg.style.display = 'block';
+            fileInput.value = '';
+        } else {
+            errorMsg.style.display = 'none';
+        }
+    });
+    </script>
+    <script>
+        CKEDITOR.replace('pesan', {
+            toolbar: [
+                { name: 'basicstyles', items: ['Bold', 'Italic', 'Underline'] },
+                { name: 'paragraph', items: ['NumberedList', 'BulletedList'] },
+                { name: 'tools', items: ['Maximize', 'Preview'] }
+            ],
+            height: 300
+        });
+    </script>
 </section>
 </body>
 </html>
