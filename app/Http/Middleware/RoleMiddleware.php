0b597c74e020764eac880134b3695f18c51b1ebe
Niyyuuu <nioghoniyyu@gmail.com>
2024-11-27 18:27:40 +0700

    commit

--- app/Http/Middleware/RoleMiddleware.php
+++ app/Http/Middleware/RoleMiddleware.php
@@ -13,7 +13,7 @@ class RoleMiddleware
         // Check if the user is authenticated and has the required role
         if (!Auth::check() || !in_array(Auth::user()->role, $roles)) {
             
-            return redirect('/home')->with('error', 'You do not have access to this page.');
+            return redirect('/home')->with('error', 'Anda tidak memiliki hak akses untuk halaman ini.');
         }
         return $next($request);
     }
